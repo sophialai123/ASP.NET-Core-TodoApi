bd1e7eede67a93d1051f835c420014f7c6ee6712
Sophia <sophia852686@gmail.com>
2023-01-08 23:58:41 +1100

    add todoitemDTO

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -8,6 +8,20 @@ namespace TodoAPI.Models
         public long Id { get; set; }
 		public string? Name { get; set; }
 		public bool IsComplete { get; set; }
-	}
+
+        //The secret field needs to be hidden from this app,
+        //but an administrative app could choose to expose it.
+        public string? Secret { get; set; }
+    }
+
+
+    //Production apps typically limit the data that's input and returned using a subset of the model(DTO).
+    //There are multiple reasons behind this, and security is a major one.
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
 }
 
